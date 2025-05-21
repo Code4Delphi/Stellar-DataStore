1ab39f84510b10de81b9c94f2f7e8fb41dc06d6b
César Cardoso <contato@code4delphi.com.br>
2025-05-20 23:20:06 -0300

    bk/2025-05-20-2320

--- FMX/Src/Main.View.pas
+++ FMX/Src/Main.View.pas
@@ -21,14 +21,22 @@ uses
   FMX.TMSFNCCustomComponent,
   FMX.Layouts,
   FMX.ListBox,
-  Products.View,
-
-  FMX.Surfaces, System.IOUtils,
-
-  FMX.DialogService.Async, System.Messaging, FMX.Objects;
+  System.Messaging,
+  FMX.Objects,
+  FMX.TabControl,
+  FMX.DateTimeCtrls;
 
 type
   TMainView = class(TForm)
+    TMSFNCCloudStellarDataStoreConnection1: TTMSFNCCloudStellarDataStoreConnection;
+    TMSFNCCloudStellarDataStoreDataSetFMX1: TTMSFNCCloudStellarDataStoreDataSetFMX;
+    DataSource1: TDataSource;
+    pnHeader: TPanel;
+    ImageLogo: TImage;
+    Label2: TLabel;
+    TabControl1: TTabControl;
+    tabAuthentication: TTabItem;
+    tabProducts: TTabItem;
     gBoxAuthentication: TGroupBox;
     lbAccessToken: TLabel;
     edtAccessToken: TEdit;
@@ -39,32 +47,41 @@ type
     Panel1: TPanel;
     btnConnect: TButton;
     btnDisconnect: TButton;
-    TMSFNCCloudStellarDataStoreConnection1: TTMSFNCCloudStellarDataStoreConnection;
-    TMSFNCCloudStellarDataStoreDataSetFMX1: TTMSFNCCloudStellarDataStoreDataSetFMX;
-    DataSource1: TDataSource;
+    TabControlProducts: TTabControl;
+    tabList: TTabItem;
+    tabRegister: TTabItem;
+    gBoxProducts: TGroupBox;
+    ListBox1: TListBox;
     btnFooter: TPanel;
     Label1: TLabel;
     lbCount: TLabel;
-    gBoxProducts: TGroupBox;
-    ListBox1: TListBox;
     btnAdd: TButton;
     btnRefresh: TButton;
-    pnHeader: TPanel;
-    Image1: TImage;
-    Label2: TLabel;
-    pnBody: TPanel;
     Image2: TImage;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    btnSave: TButton;
+    btnCancel: TButton;
+    Label3: TLabel;
+    edtPrice: TEdit;
+    Label4: TLabel;
+    edtName: TEdit;
+    Label5: TLabel;
+    edtDate: TDateEdit;
+    Edit1: TEdit;
     procedure btnConnectClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure DataSource1StateChange(Sender: TObject);
-    procedure btnAddClick(Sender: TObject);
     procedure btnDisconnectClick(Sender: TObject);
     procedure btnRefreshClick(Sender: TObject);
+    procedure btnSaveClick(Sender: TObject);
+    procedure btnCancelClick(Sender: TObject);
   private
     procedure ConfigScreen;
     procedure PreencherListBox;
     procedure SalvarImagemDoTImage;
     procedure CarregarImagemParaTImage;
+    procedure RefreshProducts;
   public
 
   end;
@@ -80,6 +97,8 @@ implementation
 procedure TMainView.FormCreate(Sender: TObject);
 begin
   ListBox1.Clear;
+  TabControlProducts.ActiveTab := tabList;
+  TabControl1.ActiveTab := tabAuthentication;
   Self.ConfigScreen;
 
   edtAccessToken.Text := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJhY2Nlc3MtdG9rZW4iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllci10b2tlbiI6IjU5MjQzOWY2LThjYjYtNDhiZS0xMzM4LTA4ZGQ5MGZjZGQ4NCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyLXByb2plY3QiOiIxOGY3MDJiNy1lOGFkLTRlOWQtZDJhZC0wOGRkOTBlYmJhZGEiLCJleHAiOjE3NDgxMTY1NDAsImlzcyI6Imh0dHBzOi8vc3RlbGxhcmRzLmlvIiwiYXVkIjoiaHR0cHM6Ly9hcGkuc3RlbGxhcmRzLmlvIn0.YDWuGl-Y7Cp3ThHRj0NtZxdpQikreNtDe7s0oOtJHHA';
@@ -116,19 +135,26 @@ begin
     raise Exception.Create('Not connected. Check the information provided.');
 
   Self.ConfigScreen;
-
+  TabControl1.ActiveTab := tabProducts;
   Self.PreencherListBox;
 end;
 
 procedure TMainView.btnDisconnectClick(Sender: TObject);
 begin
+  ListBox1.Items.Clear;
   TMSFNCCloudStellarDataStoreDataSetFMX1.Active := False;
   Self.ConfigScreen;
 end;
 
 procedure TMainView.btnRefreshClick(Sender: TObject);
 begin
+  Self.RefreshProducts;
+end;
+
+procedure TMainView.RefreshProducts;
+begin
   TMSFNCCloudStellarDataStoreDataSetFMX1.Refresh;
+  Self.PreencherListBox;
 end;
 
 procedure TMainView.PreencherListBox;
@@ -157,17 +183,12 @@ begin
   end;
 end;
 
-procedure TMainView.btnAddClick(Sender: TObject);
-begin
-  Self.SalvarImagemDoTImage;
-end;
-
 procedure TMainView.SalvarImagemDoTImage;
 var
   Stream: TMemoryStream;
   BlobStream: TStream;
 begin
-  if Image1.Bitmap.IsEmpty then
+  if ImageLogo.Bitmap.IsEmpty then
   begin
     ShowMessage('Nenhuma imagem carregada.');
     Exit;
@@ -176,7 +197,7 @@ begin
   // Salvar a imagem do TImage em memória
   Stream := TMemoryStream.Create;
   try
-    Image1.Bitmap.SaveToStream(Stream);
+    ImageLogo.Bitmap.SaveToStream(Stream);
     Stream.Position := 0;
 
     // Inserir no DataSet
@@ -210,7 +231,7 @@ begin
   // Verifica se o campo 'Image' não está vazio
   if TMSFNCCloudStellarDataStoreDataSetFMX1.FieldByName('Image').IsNull then
   begin
-    Image1.Bitmap := nil;
+    ImageLogo.Bitmap := nil;
     Exit;
   end;
 
@@ -221,13 +242,69 @@ begin
     if Stream.Size > 0 then
     begin
       Stream.Position := 0;
-      Image1.Bitmap.LoadFromStream(Stream);
+      ImageLogo.Bitmap.LoadFromStream(Stream);
     end
     else
-      Image1.Bitmap := nil;
+      ImageLogo.Bitmap := nil;
   finally
     Stream.Free;
   end;
+end;
+
+procedure TMainView.btnSaveClick(Sender: TObject);
+var
+  LMemoryStream: TMemoryStream;
+  LBlobStream: TStream;
+begin
+  if not TMSFNCCloudStellarDataStoreDataSetFMX1.Active then
+  begin
+    ShowMessage('Dataset is not open');
+    Exit;
+  end;
+
+  if Trim(edtName.Text).IsEmpty then
+  begin
+    edtName.SetFocus;
+    ShowMessage('Enter the name field');
+    Exit;
+  end;
+
+  TMSFNCCloudStellarDataStoreDataSetFMX1.Append;
+  TMSFNCCloudStellarDataStoreDataSetFMX1.FieldByName('Name').AsString := edtName.Text;
+  TMSFNCCloudStellarDataStoreDataSetFMX1.FieldByName('Price').AsFloat := StrToFloatDef(edtPrice.Text, 0);
+
+  if ImageLogo.Bitmap.IsEmpty then
+  begin
+    TMSFNCCloudStellarDataStoreDataSetFMX1.Post;
+    Exit;
+  end;
+
+  //RECORD IMAGE
+  LMemoryStream := TMemoryStream.Create;
+  try
+    ImageLogo.Bitmap.SaveToStream(LMemoryStream);
+    LMemoryStream.Position := 0;
+
+    LBlobStream := TMSFNCCloudStellarDataStoreDataSetFMX1.CreateBlobStream(
+      TMSFNCCloudStellarDataStoreDataSetFMX1.FieldByName('Image'), bmWrite);
+    try
+      LBlobStream.CopyFrom(LMemoryStream, LMemoryStream.Size);
+    finally
+      LBlobStream.Free;
+    end;
+
+    TMSFNCCloudStellarDataStoreDataSetFMX1.Post;
+  finally
+    LMemoryStream.Free;
+  end;
+
+  TabControlProducts.ActiveTab := tabList;
+  Self.RefreshProducts;
+end;
+
+procedure TMainView.btnCancelClick(Sender: TObject);
+begin
+  //
 end;
 
 end.
--- FMX/StellarDataStoreFMX.dpr
+++ FMX/StellarDataStoreFMX.dpr
@@ -3,14 +3,12 @@ program StellarDataStoreFMX;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Main.View in 'Src\Main.View.pas' {MainView},
-  Products.View in 'Src\Products.View.pas' {ProductsView};
+  Main.View in 'Src\Main.View.pas' {MainView};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TMainView, MainView);
-  Application.CreateForm(TProductsView, ProductsView);
   Application.Run;
 end.
